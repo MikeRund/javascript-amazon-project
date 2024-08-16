b4648165b4aa0b6be00d9060a341a4afc05cd607
MikeRund <mikerundle1999.MR@gmail.com>
2024-08-16 15:40:46 +0100

    Add to cart interactivity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -19,7 +19,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-price">
-            $${(product.price / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
@@ -44,13 +44,38 @@ products.forEach((product) => {
             Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary
+            js-add-to-cart"
+            data-product-id="${product.id}">
             Add to Cart
             </button>
       </div>
     `;
 });
 
-console.log(productsHTML);
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+
+            let matchingItem;
+
+            cart.forEach((item) => {
+                if (productId === item.productId) {
+                    matchingItem = item;
+                }
+            });
+
+            if (matchingItem) {
+                matchingItem.quantity++;
+            } else {
+                cart.push({
+                    productId: productId,
+                    quantity: 1
+                });
+            }
+            console.log(cart);
+        })
+    })
