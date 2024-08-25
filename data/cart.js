ee777fe032b14f6d4c16e90cdf7cd68ab6718b4f
MikeRund <mikerundle1999.MR@gmail.com>
2024-08-25 17:42:17 +0100

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,15 @@
-export let cart = [{
-    id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-}, {
-    id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart =
+        [{
+            id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2
+        }, {
+            id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1
+        }];
+}
 
 export function addToCart(productId, quantity) {
     let matchingItem;
@@ -19,10 +24,15 @@ export function addToCart(productId, quantity) {
         matchingItem.quantity += quantity;
     } else {
         cart.push({
-            productId,
+            id: productId,
             quantity
         });
     }
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 export function removeFromCart(productId) {
@@ -34,4 +44,5 @@ export function removeFromCart(productId) {
         }
     });
     cart = newCart;
+    saveToStorage();
 }
