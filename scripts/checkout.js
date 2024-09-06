8ff13884fd4ab427b432d476a535f970c9dc102c
MikeRund <mikerundle1999.MR@gmail.com>
2024-09-06 17:13:32 +0100

    Converted checkout.js from callback to promise

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,15 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 
-loadProducts(() => {
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+}).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
     renderCheckoutHeader();
 });
+
+// loadProducts(() => {
+// });
